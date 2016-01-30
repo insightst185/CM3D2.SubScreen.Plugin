b928b7afdc007ae7cbecd3f3f6ec2d6885de2762
insightst185 <insightfc3sst185@gmail.com>
2016-01-30 21:24:01 +0900

    version 0.3.9.6

--- CM3D2.SubScreen.Plugin.cs
+++ CM3D2.SubScreen.Plugin.cs
@@ -15,10 +15,10 @@ namespace CM3D2.SubScreen.Plugin
     PluginFilter("CM3D2x86"),
     PluginFilter("CM3D2VRx64"),
     PluginName("CM3D2 OffScreen"),
-    PluginVersion("0.3.9.5")]
+    PluginVersion("0.3.9.6")]
     public class SubScreen : PluginBase
     {
-        public const string Version = "0.3.9.5";
+        public const string Version = "0.3.9.6";
 
         public readonly string WinFileName = Directory.GetCurrentDirectory() + @"\UnityInjector\Config\SubScreen.png";
 
@@ -1131,7 +1131,7 @@ namespace CM3D2.SubScreen.Plugin
                     float value = ssParam.fValue[key][prop];
                     float vmin = ssParam.fVmin[key][prop];
                     float vmax = ssParam.fVmax[key][prop];
-                    string label = ssParam.sLabel[key][prop] + " : " + value.ToString("F");
+                    string label = ssParam.sLabel[key][prop] + " : ";
                     string vType = ssParam.sVType[key][prop];
 
                     outRect.width = conRect.width;
@@ -1879,8 +1879,12 @@ namespace CM3D2.SubScreen.Plugin
         {
             float conWidth = outRect.width;
 
-            outRect.width = conWidth * 0.3f;
+            outRect.width = conWidth * 0.18f;
             GUI.Label(outRect, label, lstyle);
+            outRect.x += outRect.width;
+
+            outRect.width = conWidth * 0.12f;
+            value = float.Parse(GUI.TextField(outRect,  value.ToString("F")));
             outRect.x += outRect.width;
 
             outRect.width = conWidth * 0.7f;
